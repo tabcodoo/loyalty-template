1466c6cbfbdb2dd52f28d2c8aa572fbfb23a1686
Mahir Kadic <kadic.mahir.pro@gmail.com>
2022-06-05 16:01:43 +0200

    Removed gulp and templated, removed reverse client id

--- template/templates/store/types.ts
+++
@@ -1,4 +0,0 @@
-export const <%= name.toUpperCase() %> = '<%= name %>';
-export const <%= name.toUpperCase() %>_REQUEST = '<%= name %>_request';
-export const <%= name.toUpperCase() %>_SUCCESS = '<%= name %>_success';
-export const <%= name.toUpperCase() %>_FAILURE = '<%= name %>_failure'; 
